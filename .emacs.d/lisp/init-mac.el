42403dffe41e6051d05aa32e9aa7c6f769e9c2d2
Slava Kim <slava@meteor.com>
2015-07-02 00:32:37 -0700

    Use SCP

--- .emacs.d/lisp/init-mac.el
+++ .emacs.d/lisp/init-mac.el
@@ -2,9 +2,9 @@
 (require 'init-fonts)
 
 (when (memq window-system '(mac ns))
-  (add-to-list 'default-frame-alist '(font . "Menlo-15"))
-  (set-face-attribute 'default t :font "Menlo-15")
-  (sanityinc/set-frame-font-size 14)
+  (add-to-list 'default-frame-alist '(font . "Source Code Pro-13"))
+  (set-face-attribute 'default t :font "Source Code Pro-1")
+  (sanityinc/set-frame-font-size 13)
   (define-key global-map (kbd "<s-return>") 'toggle-frame-fullscreen))
 
 (provide 'init-mac)
