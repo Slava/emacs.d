cf0a2b8e8897ea25f31c61849b2b7861851d41dd
Slava Kim <slava@meteor.com>
2015-07-03 00:49:08 -0700

    Extend Evil bindings to commit mode of Magit

--- .emacs.d/lisp/init-evil.el
+++ .emacs.d/lisp/init-evil.el
@@ -46,6 +46,7 @@
   (evil-set-initial-state 'magit-status-mode 'normal)
   (evil-set-initial-state 'magit-diff-mode 'normal)
   (evil-set-initial-state 'magit-log-mode 'normal)
+  (evil-set-initial-state 'magit-commit-mode 'normal)
   (evil-define-key 'normal magit-mode-map
     "j" 'magit-goto-next-section
     "k" 'magit-goto-previous-section)
@@ -53,6 +54,9 @@
     "j" 'magit-goto-next-section
     "k" 'magit-goto-previous-section)
   (evil-define-key 'normal magit-diff-mode-map
+    "j" 'magit-goto-next-section
+    "k" 'magit-goto-previous-section)
+  (evil-define-key 'normal magit-commit-mode-map
     "j" 'magit-goto-next-section
     "k" 'magit-goto-previous-section)
 
